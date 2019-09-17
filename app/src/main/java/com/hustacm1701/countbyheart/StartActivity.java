740b929721b425f11d6edec61758ce0e6e417f59
kingqi <2994219861@qq.com>
2019-09-17 21:42:21 +0800

    优化启动界面

---
+++ app/src/main/java/com/hustacm1701/countbyheart/StartActivity.java
@@ -0,0 +1,49 @@
+package com.hustacm1701.countbyheart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class StartActivity extends AppCompatActivity {
+    private ImageView imageView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        imageView = findViewById(R.id.start_view);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.5f,1.0f);
+        alphaAnimation.setDuration(2000);
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(StartActivity.this,MainActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        imageView.startAnimation(alphaAnimation);
+        Window window = getWindow();//设置系统栏是否适应的
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+}
