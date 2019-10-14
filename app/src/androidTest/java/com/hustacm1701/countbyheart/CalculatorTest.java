3fa65a5c118c4fd52802039372eed11fcd6d6306
edwin628 <1143934303@qq.com>
2019-10-14 19:50:03 +0800

    add some test case

--- app/src/androidTest/java/com/hustacm1701/countbyheart/CalculatorTest.java
+++ app/src/androidTest/java/com/hustacm1701/countbyheart/CalculatorTest.java
@@ -28,6 +28,59 @@ public class CalculatorTest {
         symStrList.add("+-=");
         numListList.add(getList("1","2","3"));
         answerLsit.add(1.0+2-3);
+
+        // +-负数
+        symStrList.add("+-=");
+        numListList.add(getList("1","2","4"));
+        answerLsit.add(1.0+2-4);
+
+        // +正数
+        symStrList.add("+=");
+        numListList.add(getList("6","2"));
+        answerLsit.add(6+2);
+
+        // x正数
+        symStrList.add("*=");
+        numListList.add(getList("6","2"));
+        answerLsit.add(6*2);
+
+        // x负数
+        symStrList.add("*=");
+        numListList.add(getList("-2","2"));
+        answerLsit.add(-2*2);
+
+        // div负数
+        symStrList.add("*=");
+        numListList.add(getList("40","8"));
+        answerLsit.add(40/8);
+
+        // x&- zero
+        symStrList.add("*-=");
+        numListList.add(getList("1","2","3"));
+        answerLsit.add(1*2-3);
+
+        // div&-负数
+        symStrList.add("/-=");
+        numListList.add(getList("8","4","4"));
+        answerLsit.add(8/4-4);
+
+        // mul&div
+        symStrList.add("*/=");
+        numListList.add(getList("4","9","3"));
+        answerLsit.add(4*9/3);
+
+        // mul&-负数
+        symStrList.add("*-=");
+        numListList.add(getList("4","9","38"));
+        answerLsit.add(4*9-38);
+
+        // mul&-负数
+        symStrList.add("*-=");
+        numListList.add(getList("4","9","38"));
+        answerLsit.add(4*9-38);
+
+
+
 //--------------------------------------------------
 
         for (int i = 0;i<symStrList.size();i++){
